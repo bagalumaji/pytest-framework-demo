a6ec5ab4aa9ea35167b466458c44ab7c350f950f
umaji bagal <bagalumaji@gmail.com>
2024-12-28 08:50:27 +0530

    added markers or tags

--- test_demo/test_samples.py
+++ test_demo/test_samples.py
@@ -1,10 +1,15 @@
+import pytest
+
+
+@pytest.mark.smoke
 def test_one():
     print("one")
 
-
+@pytest.mark.regression
 def test_two():
     print("two")
 
-
+@pytest.mark.login
+@pytest.mark.login1
 def test_three():
     print("three")
