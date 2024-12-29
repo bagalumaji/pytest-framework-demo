58ac71edc6c3241ad4d892b9ea71852e1b59c578
umaji bagal <bagalumaji@gmail.com>
2024-12-29 08:11:16 +0530

    fixture updated

--- fixtures/test_setup_fixture.py
+++ fixtures/test_setup_fixture.py
@@ -2,9 +2,11 @@ import pytest
 
 
 @pytest.fixture
-def setup():
+def setup_and_teardown():
     print("setup fixture executed")
+    yield
+    print("tear down executed")
 
 
-def test_setup_fixture(setup):
+def test_setup_fixture(setup_and_teardown):
     print("test executed")
