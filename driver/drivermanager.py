324675dcb8bc873e7b45271f2e4a93206a013475
umaji bagal <bagalumaji@gmail.com>
2024-12-31 21:49:46 +0530

    created driver manager class

---
+++ driver/drivermanager.py
@@ -0,0 +1,17 @@
+from threading import Thread
+
+
+
+class DriverManager:
+    # _driver =
+    @staticmethod
+    def get_driver():
+        pass
+
+    @staticmethod
+    def set_driver(driver):
+        pass
+
+    @staticmethod
+    def unload():
+        pass
