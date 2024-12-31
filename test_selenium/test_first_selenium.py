f97300345eb898faa8f9eab2f3b837965e0b97f7
umaji bagal <bagalumaji@gmail.com>
2024-12-31 10:30:51 +0530

    updated test

--- test_selenium/test_first_selenium.py
+++ test_selenium/test_first_selenium.py
@@ -1,7 +1,12 @@
 import pytest
 
+from pages.home_page import HomePage
+
 
 @pytest.mark.usefixtures('setup_and_teardown_browser')
 class TestDemo:
+
     def test_first_demo(self):
-        print(self.driver.title)
+        home_page = HomePage(self.driver)
+        home_page.click_on_my_account()
+        home_page.click_on_login()
