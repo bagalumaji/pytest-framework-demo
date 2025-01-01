107db6381501a1cfb24f3243208dda429ba245c2
umaji bagal <bagalumaji@gmail.com>
2025-01-01 12:08:09 +0530

    updated driver

--- driver/driver.py
+++ driver/driver.py
@@ -6,10 +6,13 @@ from factory.driverfactory import DriverFactory
 class Driver:
     @staticmethod
     def init_driver(browser_name: Browser):
-        DriverManager.set_driver(DriverFactory.get_driver(browser_name))
-        DriverManager.get_driver().get("https://tutorialsninja.com/demo/index.php")
+        if not DriverManager.get_driver():
+            DriverManager.set_driver(DriverFactory.get_driver(browser_name))
+            DriverManager.get_driver().maximize_window()
+            DriverManager.get_driver().get("https://tutorialsninja.com/demo/index.php")
 
     @staticmethod
     def close_driver():
-        DriverManager.get_driver().quit()
-        DriverManager.unload()
+        if driver := DriverManager.get_driver():
+            driver.quit()
+            DriverManager.unload()
