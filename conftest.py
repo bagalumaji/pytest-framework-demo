692b65fb9b43cec833f2b9fa0bc21521b1c6a561
umaji bagal <bagalumaji@gmail.com>
2025-01-01 11:57:02 +0530

    updated with enums

--- conftest.py
+++ conftest.py
@@ -1,11 +1,13 @@
 import pytest
 
 from driver.driver import Driver
+from enums.browser import Browser
+from exceptions.browser_not_supported_exception import BrowserNotSupportedError
 
 
 @pytest.fixture()
 def setup_and_teardown_browser(request):
-    browser_name = request.config.getoption('browser')
+    browser_name = get_browser(request.config.getoption('browser'))
     Driver.init_driver(browser_name)
     yield
     Driver.close_driver()
@@ -13,4 +15,13 @@ def setup_and_teardown_browser(request):
 
 def pytest_addoption(parser):
     browser_name = "chrome"
-    parser.addoption("--browser", action="store", default=browser_name, help="provide browser name as - chrome, firefox...")
+    parser.addoption("--browser", action="store", default=browser_name,
+                     help="provide browser name as - chrome, firefox...")
+
+
+def get_browser(browser_name) -> Browser:
+    try:
+        browser = Browser(browser_name)
+        return browser
+    except Exception:
+        raise BrowserNotSupportedError(browser_name)
