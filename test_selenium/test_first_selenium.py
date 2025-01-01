b0b41573ab9679e2601936f888dfd2010f32730b
umaji bagal <bagalumaji@gmail.com>
2025-01-01 14:44:39 +0530

    updated test

--- test_selenium/test_first_selenium.py
+++ test_selenium/test_first_selenium.py
@@ -1,12 +1,13 @@
+import time
+
 import pytest
 
-from pages.home_page import HomePage
+from driver.drivermanager import DriverManager
 
 
 @pytest.mark.usefixtures('setup_and_teardown_browser')
 class TestDemo:
 
     def test_first_demo(self):
-        home_page = HomePage(self.driver)
-        home_page.click_on_my_account()
-        home_page.click_on_login()
+        print(DriverManager.get_driver().title)
+        time.sleep(3)
