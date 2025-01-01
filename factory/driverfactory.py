3e74ad80ae94a7d7b2ff291ea65faead027850b8
umaji bagal <bagalumaji@gmail.com>
2025-01-01 11:57:11 +0530

    updated with enums

--- factory/driverfactory.py
+++ factory/driverfactory.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 
+from enums.browser import Browser
 from exceptions.browser_not_supported_exception import BrowserNotSupportedError
 
 
 class DriverFactory:
-    _browser_dict = {"chrome": lambda: webdriver.Chrome(), "firefox": lambda: webdriver.Firefox()}
+    _browser_dict = {Browser.CHROME: lambda: webdriver.Chrome(), Browser.FIREFOX: lambda: webdriver.Firefox()}
 
     @staticmethod
-    def get_driver(browser_name):
+    def get_driver(browser_name: Browser):
         if browser_name not in DriverFactory._browser_dict:
             raise BrowserNotSupportedError(browser_name)
 
