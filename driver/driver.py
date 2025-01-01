6072384528f27dcd107c8971fa08c3b0f6ac7aa5
umaji bagal <bagalumaji@gmail.com>
2025-01-01 11:57:06 +0530

    updated with enums

--- driver/driver.py
+++ driver/driver.py
@@ -1,10 +1,11 @@
 from driver.drivermanager import DriverManager
+from enums.browser import Browser
 from factory.driverfactory import DriverFactory
 
 
 class Driver:
     @staticmethod
-    def init_driver(browser_name):
+    def init_driver(browser_name: Browser):
         DriverManager.set_driver(DriverFactory.get_driver(browser_name))
         DriverManager.get_driver().get("https://tutorialsninja.com/demo/index.php")
 
