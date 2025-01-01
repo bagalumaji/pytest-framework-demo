83e50750bd64775d217e8dc29371480b8b6905d0
umaji bagal <bagalumaji@gmail.com>
2025-01-01 12:08:15 +0530

    updated drivermanager

--- driver/drivermanager.py
+++ driver/drivermanager.py
@@ -1,20 +1,20 @@
 import threading
 
-from selenium.webdriver.ie.webdriver import WebDriver
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class DriverManager:
-    _thread_local_driver:WebDriver = threading.local()
+    _thread_local_driver: WebDriver = threading.local()
 
     @staticmethod
-    def get_driver():
-        return getattr(DriverManager._thread_local_driver,"driver",None)
+    def get_driver() -> WebDriver:
+        return getattr(DriverManager._thread_local_driver, "driver", None)
 
     @staticmethod
-    def set_driver(driver):
+    def set_driver(driver: WebDriver):
         DriverManager._thread_local_driver.driver = driver
 
     @staticmethod
     def unload():
-        if hasattr(DriverManager._thread_local_driver,"driver"):
+        if hasattr(DriverManager._thread_local_driver, "driver"):
             del DriverManager._thread_local_driver
