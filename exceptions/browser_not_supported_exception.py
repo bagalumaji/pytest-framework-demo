0bfe85ecfff1d81edcf52bd24d177d986d167f0d
umaji bagal <bagalumaji@gmail.com>
2025-01-01 11:56:49 +0530

    updated message

--- exceptions/browser_not_supported_exception.py
+++ exceptions/browser_not_supported_exception.py
@@ -1,3 +1,6 @@
+from enums.browser import Browser
+
+
 class BrowserNotSupportedError(RuntimeError):
     def __init__(self, browser_name):
-        super().__init__(f"'{browser_name}' is not a supported browser.")
+        super().__init__(f"'{browser_name}' is not a valid Browser")
