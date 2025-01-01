3be2722a13b4ea14fc877b000741fea8e6a42d28
umaji bagal <bagalumaji@gmail.com>
2025-01-01 14:44:50 +0530

    added test

--- test_selenium/test_second.py
+++ test_selenium/test_second.py
@@ -1,8 +1,16 @@
 import time
 
+import pytest
+
 from driver.drivermanager import DriverManager
 
 
-def test_second_selenium(setup_and_teardown_browser):
-    print(DriverManager.get_driver().title)
-    time.sleep(3)
+@pytest.mark.usefixtures('setup_and_teardown_browser')
+class TestSecond:
+    def test_second_selenium(self):
+        print(DriverManager.get_driver().title)
+        time.sleep(3)
+
+    def test_second_selenium_1(self):
+        print(DriverManager.get_driver().title)
+        time.sleep(3)
